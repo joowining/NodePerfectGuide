a59c8ba71a3c308e1963eff24bbfc9919f2ec3d5
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-03-27 18:36:20 +0900

    initial commit, till section 8

---
+++ controllers/shop.js
@@ -0,0 +1,46 @@
+const Product = require('../models/product');
+
+exports.getProducts = (req, res, next) => {
+    Product.fetchAll(products => {
+      res.render('shop/product-list', {
+        prods: products,
+        pageTitle: 'Shop',
+        path: '/',
+        hasProducts: products.length > 0,
+        activeShop: true,
+        productCSS: true
+      });
+    });
+  };
+
+
+exports.getIndex = (req, res, next)=>{
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+}
+
+exports.getCart = (req, res, next) =>{
+  res.render('shop/cart',{
+    prods :'/cart',
+    pageTitle : 'Your carts'
+  })
+}
+
+exports.getOrders = (req, res, next) =>{
+  res.render('shop/orders',{
+    prods :'/orders',
+    pageTitle : 'Your Orderss'
+  })
+}
+
+exports.getCheckout = ( req, res , nect) =>{
+  res.render('shop/checkout',{
+    path:'/checkout',
+    pageTitle:'Checkout'
+  })
+}
