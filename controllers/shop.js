b4482a0943cbcdff5ca1f2f1bbd892f7fc7ddb0f
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-03-30 12:59:48 +0900

    data access from url

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,6 +13,13 @@ exports.getProducts = (req, res, next) => {
     });
   };
 
+exports.getProduct = (req,res,next)=>{
+  //access id in url /products/:productId
+  const prodId = req.params.productId;
+  console.log(prodId);
+  res.redirect('/');
+}
+
 
 exports.getIndex = (req, res, next)=>{
   Product.fetchAll(products => {
--- routes/shop.js
+++ routes/shop.js
@@ -10,6 +10,9 @@ router.get('/', shopController.getIndex);
 
 router.get('/products',shopController.getProducts);
 
+//dynamic segment ruotuer. in this case order of router is important.
+router.get('/products/:productId',shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
