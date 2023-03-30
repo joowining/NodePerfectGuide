03b847fae2302e366904904cabc3e550454a55e8
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-03-30 13:59:18 +0900

    implement cart mdoel, controller

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
     Product.fetchAll(products => {
@@ -43,6 +44,14 @@ exports.getCart = (req, res, next) =>{
     pageTitle : 'Your carts'
   })
 }
+
+exports.postCart = (req,res,next) =>{
+  const prodId = req.body.productId;
+  Product.findById(prodId, (product)=>{
+    Cart.addProduct(prodId, product.price);
+  })
+  res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) =>{
   res.render('shop/orders',{
