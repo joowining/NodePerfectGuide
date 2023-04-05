086e3b3f77e5de1b6f5ed8302603749221abb7eb
JoowonKim <99549623+joowining@users.noreply.github.com>
2023-04-05 21:06:08 +0900

    little change

--- models/cart.js
+++ models/cart.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-    path.dirname(Process.mainModule.filename),
+    path.dirname(process.mainModule.filename),
     'data',
     'cart.json'
 )
